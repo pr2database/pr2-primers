549d828b919cd3b37629c2aa253cf68b2e9f3181
Daniel Vaulot <vaulot@gmail.com>
2020-07-02 15:58:38 +0800

    Shiny

---
+++ shiny/app.R
@@ -0,0 +1,50 @@
+library(shiny)
+library(DT)
+
+
+primers<- readRDS("primers.rds")
+primer_sets<- readRDS("primer_sets.rds") 
+
+
+ui <- fluidPage(
+  title = "Primer database",
+  titlePanel("Primer database"),
+  sidebarLayout(
+    sidebarPanel(width = 3,
+      conditionalPanel(
+        'input.dataset === "Primers"',
+        checkboxGroupInput("show_vars_table1", "Columns to show:",
+                           names(primers), selected = names(primers))
+      ),
+      conditionalPanel(
+        'input.dataset === "Primer sets"',
+        checkboxGroupInput("show_vars_table2", "Columns to show:",
+                           names(primer_sets), selected = names(primer_sets))
+      ),
+    ),
+    mainPanel(
+      tabsetPanel(
+        id = 'dataset',
+        tabPanel("Primers", DT::dataTableOutput("mytable1")),
+        tabPanel("Primer sets", DT::dataTableOutput("mytable2"))
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+
+  # choose columns to display
+  output$mytable1 <- DT::renderDataTable({
+    DT::datatable(primers[, input$show_vars_table1, drop = FALSE])
+  })
+
+  # choose columns to display
+  output$mytable2 <- DT::renderDataTable({
+    DT::datatable(primer_sets[, input$show_vars_table2, drop = FALSE])
+  })
+
+
+}
+
+shinyApp(ui, server)
